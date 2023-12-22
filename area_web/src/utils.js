14fc033f7df7d4f2c1135b35ed99e8da9dadf12e
david <david.benistant@epitech.eu>
2023-12-22 16:40:44 +0100

    [+] change api url for prod

--- area_web/src/utils.js
+++ area_web/src/utils.js
@@ -1,3 +1,4 @@
 // export let API_URL = "http://35.181.66.104:8080";
 
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
+export const API_URL = "https://ckdetori3btxbnfqr5gjnvab4i0sbznl.lambda-url.eu-west-3.on.aws";
