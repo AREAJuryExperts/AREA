a38494a7f8d70c4adad367ee2e4748726325c998
david <david.benistant@epitech.eu>
2023-12-23 10:42:11 +0100

    [+] add cors header

--- area_web/src/LoginPage/LoginPage.js
+++ area_web/src/LoginPage/LoginPage.js
@@ -49,6 +49,7 @@ export default function Login() {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
             },
             body: JSON.stringify({
                 email: email,
