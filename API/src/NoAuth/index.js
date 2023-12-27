0f0f75246722a561094f9d35adb1c6b1389ab505
cote2porc <robin.denni@epitech.eu>
2023-12-27 14:30:05 +0100

    fix(API/Services/Asana&&NoAuth/asana)add the webhook to theindex of the noauth

--- API/src/NoAuth/index.js
+++ API/src/NoAuth/index.js
@@ -6,5 +6,5 @@ router.get("/", (req, res) => res.send({ msg: "NoAuth" }));
 router.use("/trello/", require("./Trello"));
 router.use("/github/", require("./Github"));
 router.use("/discord/", require("./Discord"));
-
+router.use("/asana/", require("./Asana"));
 module.exports = router;
