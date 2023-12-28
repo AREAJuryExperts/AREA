c9631ff911f16023bfd5d240f31083798e5e884d
david <david.benistant@epitech.eu>
2023-12-28 18:38:45 +0100

    [+H] add icon in iconRouter

--- area_web/src/utils.js
+++ area_web/src/utils.js
@@ -1,6 +1,8 @@
 import DiscordLogo from "./assets/DiscordLogo.png";
 import TrelloLogo from "./assets/TrelloLogo.png";
 import GithubLogo from "./assets/GithubLogo.png";
+import AsanaLogo from "./assets/Asana.png";
+import GoogleLogo from "./assets/Google.png";
 
 export let API_URL = "https://area.david-benistant.com:8080";
 
@@ -8,7 +10,10 @@ export let API_URL = "https://area.david-benistant.com:8080";
 export const IconRouter = (app) => {
     if (app === "Discord") return DiscordLogo;
     if (app === "Trello") return TrelloLogo;
-    if (app === "Github") return GithubLogo;
+    if (app === "GitHub") return GithubLogo;
+    if (app === "Asana") return AsanaLogo;
+    if (app === "Google") return GoogleLogo;
+    return null;
 };
 // export const API_URL = "http://localhost:8080";
 // export const API_URL = "https://ckdetori3btxbnfqr5gjnvab4i0sbznl.lambda-url.eu-west-3.on.aws";
