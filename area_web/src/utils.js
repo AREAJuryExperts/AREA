c6927a8641c6b5a8c7d5ba33f626c21563101189
david <david.benistant@epitech.eu>
2023-12-28 03:09:08 +0100

    [+H] change api url in https

--- area_web/src/utils.js
+++ area_web/src/utils.js
@@ -1,4 +1,4 @@
-export let API_URL = "http://area.david-benistant.com:8080";
+export let API_URL = "https://area.david-benistant.com:8080";
 
 // export const API_URL = "http://localhost:8080";
 // export const API_URL = "https://ckdetori3btxbnfqr5gjnvab4i0sbznl.lambda-url.eu-west-3.on.aws";
