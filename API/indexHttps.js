dce43b0a64d67c41f4fb443a6ba0e937d26077c1
david <david.benistant@epitech.eu>
2023-12-28 03:04:24 +0100

    [-H] remove api http redirect

--- API/indexHttps.js
+++ API/indexHttps.js
@@ -12,12 +12,6 @@ const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
 
 // Écoute sur le port HTTPS par défaut (443)
-httpsServer.listen(443, () => {
+httpsServer.listen(8080, () => {
     console.log('Le serveur HTTPS est en cours d\'exécution');
 });
-
-const http = require('http');
-http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(8080);
