5aec1f9f23b0595397ed67af0e2df1c3991261c4
cote2porc <robin.denni@epitech.eu>
2023-12-29 14:23:26 +0100

    [+H] rename the AsanaUser.token into AsanaUser.access_token on the reaction part of asana

--- API/src/Services/Asana/reactions.js
+++ API/src/Services/Asana/reactions.js
@@ -12,14 +12,10 @@ const AsanaCreateProject = async (user, projectName = "Project" + Math.floor(Mat
         },
     };
     let tmpUser = await db.client().query(params).promise();
-    console.log("hello 1");
     if (tmpUser.Count === 0) return null;
-    console.log("hello 2");
     let AsanaUser = tmpUser.Items[0];
     if (!AsanaUser) return null;
-    console.log("hello 3");
-    if (!AsanaUser.token) return null;
-    console.log("hello 4");
+    if (!AsanaUser.access_token) return null;
     if (projectId === -1) {
         let url = 'https://app.asana.com/api/1.0/workspaces?opt_fields=';
         let options = {
@@ -33,13 +29,10 @@ const AsanaCreateProject = async (user, projectName = "Project" + Math.floor(Mat
             let res = await fetch(url, options);
             if (res.status !== 201) {
                 let token = await refreshToken(AsanaUser.refresh_token, AsanaUser);
-                console.log("hello 5");
                 if (!token.access_token) return null;
-                console.log("hello 66");
                 AsanaUser.access_token = token.access_token;
                 res = await fetch(url, options);
                 if (res.status !== 200) return null;
-                console.log("hello 777");
             }
             let data = await res.json();
             projectId = data.data[0].id;
@@ -50,7 +43,6 @@ const AsanaCreateProject = async (user, projectName = "Project" + Math.floor(Mat
 
     }
     let url = `https://app.asana.com/api/1.0/projects`;
-    console.log("hello 888");
     try {
         let lastres = await fetch(url, {headers : {accept: 'application/json', authorization: `Bearer ${AsanaUser.access_token}`}, 
         method : "POST", body : JSON.stringify({data : {name : projectName, workspace : projectId}})})
@@ -59,7 +51,6 @@ const AsanaCreateProject = async (user, projectName = "Project" + Math.floor(Mat
         console.log(err);
         return null;
     }
-    return "not null"
 };
 
 module.exports = {AsanaCreateProject};
