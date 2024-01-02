4cb3c436581e957cccd7c84438768a5a4b613eab
cote2porc <robin.denni@epitech.eu>
2024-01-02 10:02:32 +0100

    feat(area_web) add the regsitration to the jira service

--- area_web/src/Confirm/confirmJira.js
+++ area_web/src/Confirm/confirmJira.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
+
 export default function confirmJira({})
 {
+    const getBearerToken = async () => {
+        let token = localStorage.getItem("jwt");
+        const urlParams = new URLSearchParams(window.location.search);
+        try {
+            let res = await fetch(API_URL + "/api/jira/register?code=" + urlParams.get('code'), {method : "POST", headers : {"Authorization" : token}})
+            let data = await res.json();
+            window.location.href = "/";
+            console.log(data);
+            return;
+        } catch (err) {
+            console.log(err);
+            return;
+        }
+    }
+    useEffect(() => {
+        getBearerToken();
+    }, []);
     return (<></>)
 }
