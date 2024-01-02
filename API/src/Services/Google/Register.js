e0cf233b9814ed0dd051f416036a4ebfeb0f196c
TheotimeS <theotime.schmeltz@epitech.eu>
2024-01-02 17:27:29 +0100

    {-H} Removed useless api call in API/Google/Register

--- API/src/Services/Google/Register.js
+++ API/src/Services/Google/Register.js
@@ -12,31 +12,10 @@ const Register = async (req, res) => {
     const {scope, access_token} = req.body;
 
     if (access_token) {
-        try {
-            let me = await fetch("https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses", {
-                method: "GET",
-                mode : 'cors',
-                headers: {
-                    Authorization: `Bearer ${access_token}`,
-                },
-            });
-
-            if (me.status != 200) throw { status: 400, msg: "Invalid code" };
-            me = await me.json();
-        } catch (err) {
-            res.status(err.status).send(err.msg);
-            return;
-        }
-        if (!me) {
-            res.status(400).send({ msg: "Invalid code" });
-            return;
-        }
         let googleUser = {
             userId: req.user.id,
             access_token: access_token,
             scope: scope,
-            id: me.id,
-            login: me.login,
         };
         await dynamo
             .client()
