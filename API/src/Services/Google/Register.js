328d6583fb50947d3750f5a294986feaeddef9c0
TheotimeS <theotime.schmeltz@epitech.eu>
2024-01-02 20:38:20 +0100

    {|H} Finally fixed API/Google/Register

--- API/src/Services/Google/Register.js
+++ API/src/Services/Google/Register.js
@@ -12,18 +12,38 @@ const Register = async (req, res) => {
     const {scope, access_token} = req.body;
 
     if (access_token) {
+        let me = null;
+        try {
+            me = await fetch("https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${access_token}`,
+                },
+            });
+
+            if (me.status != 200) throw { status: 400, msg: "Invalid code" };
+            me = await me.json();
+        } catch (err) {
+            res.status(err.status).send(err.msg);
+            return;
+        }
+        if (!me) {
+            res.status(400).send({ msg: "Invalid code" });
+            return;
+        }
         let googleUser = {
             userId: req.user.id,
             access_token: access_token,
             scope: scope,
+            id: me.names[0].metadata.source.id,
         };
         await dynamo
-            .client()
-            .put({
-                TableName: "GoogleUsers",
-                Item: googleUser,
-            })
-            .promise();
+        .client()
+        .put({
+            TableName: "GoogleUsers",
+            Item: googleUser,
+        })
+        .promise();
         if (!req.user.connected) req.user.connected = [];
         req.user.connected.push("Google");
         await dynamo
