aad9fbf24f12d3b9b7f65d3c82d907ce7f40d25b
TheotimeS <theotime.schmeltz@epitech.eu>
2024-01-03 16:19:54 +0100

    {|H} Refactored google's registration to store a refresh token in db

--- API/src/Services/Google/Register.js
+++ API/src/Services/Google/Register.js
@@ -3,39 +3,57 @@ const dynamo = require("../../../DB");
 
 const Register = async (req, res) => {
     try {
-        utils.checkArgs(req.body, ["access_token", "scope"]);
+        utils.checkArgs(req.body, ["code", "scope"]);
     } catch (err) {
         res.status(err.status).send(err.msg);
         return;
     }
 
-    const {scope, access_token} = req.body;
+    const {scope, code} = req.body;
 
-    if (access_token) {
+    if (code) {
+        let token = null;
         let me = null;
+        const params = new URLSearchParams();
+        params.append("client_id", process.env.GOOGLE_CLIENT_ID);
+        params.append("client_secret", process.env.GOOGLE_CLIENT_SECRET);
+        params.append("grant_type", "authorization_code");
+        params.append("redirect_uri", process.env.GOOGLE_REDIRECT_URI);
         try {
+            token = await fetch("https://oauth2.googleapis.com/token?code=" + code, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    Accept: "application/json",
+                },
+                body: params.toString(),
+            });
+
+            if (token.status != 200) throw { status: 400, msg: "Invalid code" };
+            token = await token.json();
             me = await fetch("https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses", {
                 method: "GET",
                 headers: {
-                    Authorization: `Bearer ${access_token}`,
+                    Authorization: `Bearer ${token.access_token}`,
                 },
             });
 
-            if (me.status != 200) throw { status: 400, msg: "Invalid code" };
+            if (me.status != 200) throw { status: 400, msg: "Invalid access_token" };
             me = await me.json();
         } catch (err) {
             res.status(err.status).send(err.msg);
             return;
         }
         if (!me) {
-            res.status(400).send({ msg: "Invalid code" });
+            res.status(400).send({ msg: "Invalid access_token" });
             return;
         }
         let expiresIn = new Date();
         expiresIn.setHours(expiresIn.getHours() + 1);
         let googleUser = {
             userId: req.user.id,
-            access_token: access_token,
+            access_token: token.access_token,
+            refresh_token: token.refresh_token,
             scope: scope,
             id: me.names[0].metadata.source.id,
             expiresIn: expiresIn.toDateString(),
