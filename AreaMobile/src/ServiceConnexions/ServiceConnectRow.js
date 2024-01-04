29c3d2775da18904b8e35874864cf03d4c8a17cc
cote2porc <robin.denni@epitech.eu>
2024-01-04 16:29:16 +0100

    fix(AreaMobile/src) fix the scroll to the Text input on the login page, remove some of the annoying console.log

--- AreaMobile/src/ServiceConnexions/ServiceConnectRow.js
+++ AreaMobile/src/ServiceConnexions/ServiceConnectRow.js
@@ -1,32 +1,32 @@
 import { Text, TouchableOpacity, Image, AppState } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const frontUrl = "https://area.david-benistant.com"
 
-export default function ServiceConnectRow({area, me, reloadMe, setReloadMe}) {
+export default function ServiceConnectRow({ area, me, reloadMe, setReloadMe }) {
     const [alreadyGot, setAlreadyGot] = useState(false);
-    const {app, icon, authUrl} = area;
+    const { app, icon, authUrl } = area;
     const openService = async () => {
-      let result = await WebBrowser.openBrowserAsync(frontUrl + "/confirmMobile?redirect=" + authUrl);
+        let result = await WebBrowser.openBrowserAsync(frontUrl + "/confirmMobile?redirect=" + authUrl);
     };
     useEffect(() => {
-      if (!me)
-        return;
-      for (let i in me.connected) {
-            console.log(me)
-              if (me.connected[i].toLowerCase() === app.toLowerCase())
-                  return setAlreadyGot(true);
-        }
+        if (!me)
+            return;
+        for (let i in me.connected)
+            if (me.connected[i].toLowerCase() === app.toLowerCase())
+                return setAlreadyGot(true);
     }, [me])
     return (
-        <TouchableOpacity onPress={() => {return !alreadyGot ? openService() : ''}} 
-        style={{flexDirection : 'row', alignItems : 'center', height : 'auto', width : '100%', paddingTop : 10, 
-        paddingBottom : 10, borderWidth : 1, borderLeftWidth : 0, borderRightWidth : 0, marginTop : 20}}>
-            <Image source={icon} style={{ width: 43, height: 41, marginLeft: 15, marginRight : 10 }}  />
-            <Text style={{fontSize : 20}}>{app}</Text>
-            <MaterialCommunityIcons name={alreadyGot ? 'check' : 'close'} size={30} color={alreadyGot ? 'green' : 'red'} style={{position : 'absolute', right : 10}}/>
+        <TouchableOpacity onPress={() => { return !alreadyGot ? openService() : '' }}
+            style={{
+                flexDirection: 'row', alignItems: 'center', height: 'auto', width: '100%', paddingTop: 10,
+                paddingBottom: 10, borderWidth: 1, borderLeftWidth: 0, borderRightWidth: 0, marginTop: 20
+            }}>
+            <Image source={icon} style={{ width: 43, height: 41, marginLeft: 15, marginRight: 10 }} />
+            <Text style={{ fontSize: 20 }}>{app}</Text>
+            <MaterialCommunityIcons name={alreadyGot ? 'check' : 'close'} size={30} color={alreadyGot ? 'green' : 'red'} style={{ position: 'absolute', right: 10 }} />
         </TouchableOpacity>
     )
 }
