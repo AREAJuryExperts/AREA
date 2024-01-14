2007c5bd9192822ceb53e9e68b58dcba0c23cbb2
david <david.benistant@epitech.eu>
2024-01-14 20:56:54 +0100

    [/] try fix github

--- API/src/Services/Github/Register.js
+++ API/src/Services/Github/Register.js
@@ -24,7 +24,9 @@ const Register = async (req, res) => {
     });
 
     if (data.status != 200) {
-        res.status(data.status).send({ msg: "Invalid code" });
+        data = await data.text()
+        console.log(data)
+        res.status(400).send({ msg: "Invalid code" });
         return;
     }
 
